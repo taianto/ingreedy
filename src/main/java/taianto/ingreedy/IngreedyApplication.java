ce019ca1ad33dbd06fb00a5a2c0de75916c03d19
taianto <antont21c@gmail.com>
2025-05-20 22:05:14 +0200

    Started Spring Boot setup

---
+++ src/main/java/taianto/ingreedy/IngreedyApplication.java
@@ -0,0 +1,13 @@
+package taianto.ingreedy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngreedyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngreedyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/taianto/ingreedy/IngreedyApplicationTests.java
@@ -0,0 +1,13 @@
+package taianto.ingreedy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngreedyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
